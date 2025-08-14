9b5d19b8288f7c8b1db3b7e753d38a396b20f331
Sujal Anerao <sujalkadve@gmail.com>
2025-08-15 00:43:49 +0530

    Update & enhaced script.js

--- script.js
+++ script.js
@@ -3,9 +3,25 @@ const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 
 sign_up_btn.addEventListener("click", () => {
-    container.classList.add("sign-up-mode");
+  container.classList.add("sign-up-mode");
 });
 
 sign_in_btn.addEventListener("click", () => {
-    container.classList.remove("sign-up-mode");
+  container.classList.remove("sign-up-mode");
+});
+
+// Toggle password visibility
+document.querySelectorAll(".toggle-password").forEach((icon) => {
+  icon.addEventListener("click", () => {
+    const input = icon.previousElementSibling;
+    if (input.type === "password") {
+      input.type = "text";
+      icon.classList.remove("fa-eye");
+      icon.classList.add("fa-eye-slash");
+    } else {
+      input.type = "password";
+      icon.classList.remove("fa-eye-slash");
+      icon.classList.add("fa-eye");
+    }
+  });
 });
